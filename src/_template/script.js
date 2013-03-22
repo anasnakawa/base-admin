e3583d36b44f3e1c1ebfd1e526184d6fbd195f8d
anasnakawa <anas.nakawa@gmail.com>
2013-03-22 18:19:12 +0400

    continue

---
+++ src/_template/script.js
@@ -0,0 +1,28 @@
+/*!
+ * ------------------------------
+ * library name v#.#.#
+ * http://library-url.com
+ * license: MIT license (http://opensource.org/licenses/MIT)
+ * ------------------------------
+ */
+
+// ------------------------------
+// table of content
+// ------------------------------
+// a header title
+//  - sub title
+// a method
+// ------------------------------
+
+// a header title
+// --------------
+
+// sub title
+
+// a method
+// describe your method
+//
+// * **param:** {type} paramName    what to do with this parameter
+//
+// e.g: if you want to provide an example, that would be nice
+// ------------------------------
