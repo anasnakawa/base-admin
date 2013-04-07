4338432d2639a5600d6aa05a2a277358e36287c6
anasnakawa <anas.nakawa@gmail.com>
2013-04-07 22:17:04 +0400

    off canvas layout .. the basics

--- src/documents/scripts/common/common-off-canvas-nav.js
+++ src/documents/scripts/common/common-off-canvas-nav.js
@@ -9,10 +9,26 @@
 // ------------------------------
 // ------------------------------
 
-(function( $ ) {
+// ------------------------------
+// Notes
+// ------------------------------
+// we do not need any break point scripts, we just need to bind an event
+// on the navbar button (which will only appear on tablets and below)
+// then handle the off canvas layout by toggleing a class on html / body or whatever..
+// ------------------------------
+
+(function( $, _ ) {
   
   // local vars
   var $win = $(window)
+  , breakPoint = {
+      width: 1200
+    , over: 'over'
+    , below: 'below'
+  }
+  , breakPointChanged = function( breakPoint ) {
+    return $(window).width()
+  }
   , $nav = $('[data-off-canvas="nav"]')
   , $content = $('[data-off-canvas="content"]')
   , below = 'exitBreakpoint'
@@ -20,7 +36,19 @@
   
   console.log( $nav, $content );
   
-  // initiate break points
+  // caching current break point
+  $win.data('breakpoint', $win.width >= breakPoint.width ? breakPoint.over : breakPoint.below);
+  
+  // since resize method will be fired much more than we need
+  // throttle (from underscore.js) makes sure that resize handler
+  // will be fired no more than once in every 200 miliseconds
+  $win.resize( _.throttle(function() {
+    
+    
+    
+  }, 200) );
+  
+  /* initiate break points
   $win.setBreakpoints({
       // use only largest available vs use all available
       distinct: true, 
@@ -33,6 +61,10 @@
           980,
           1200
       ] 
+  });
+  // */
+  $win.bind( below + '980 ' + over + '980', function(e) {
+    $('html').toggleClass('')
   });
   
   // where should off canvas nav hidden
@@ -50,4 +82,6 @@
     $content.css('width', null);
   });
   
-})( jQuery );
+  
+  
+})( jQuery, _ );
